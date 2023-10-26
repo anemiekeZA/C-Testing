6c075be38b8fc0fc35bd737f85d0cb93ea738bac
Anemieke <anvanv022@student.wethinkcode.co.za>
2023-10-26 08:57:49 +0200

    Adding files

--- c#Testing/secondTest/secondTestNegTest.cs
+++ c#Testing/secondTest/secondTestNegTest.cs
@@ -1,55 +1,156 @@
-public class NegativeTestScenarios
+using System.Collections;
+using Calculators;
+namespace Exercise6Tests;
+public class NegTest
 {
-    private readonly DecimalCalculator _calculator = new();
-    private static IEnumerable AdditionTest()
+    private readonly SimpleCalculator _calculator = new();
+    
+    //Addition by Max Value
+    private static IEnumerable AdditionTestData()
     {
-        yield return new TestCaseData(12m);
-        yield return new TestCaseData(31m);
-        yield return new TestCaseData(90m);
-    }
- 
-    [TestCaseSource(nameof(AdditionTestData))]
-    public void GeneratorAddition(decimal value1)
-    {
-        Assert.Throws<OverflowException>(()=>this._calculator.Enter(decimal.MaxValue).Plus(value1).Equals());
-    }
-
-    private static IEnumerable DivisionTest()
-    {
-        yield return new TestCaseData(16m);
+        yield return new TestCaseData(2m);
+        yield return new TestCaseData(3m);
         yield return new TestCaseData(30m);
-        yield return new TestCaseData(80m);
+
     }
 
-    [TestCaseSource(nameof(DivisionTest))]
-    public void GeneratorDivision(decimal value1)
+    [TestCaseSource(nameof(AdditionTestData))]
+    public void GeneratorDataDrivenAddition(decimal value1)
     {
+        // Assert
+        Assert.Throws<OverflowException>(()=>this._calculator.Enter(decimal.MaxValue).Plus(value1));
+    }
+
+//----------------------------------------------------------------------------------------------
+
+    // Addition by Min Value 
+       private static IEnumerable AdditionTestByMinValueData()
+    {
+        yield return new TestCaseData(-2.0m);
+        yield return new TestCaseData(-3.0m);
+        yield return new TestCaseData(-1.0m);
+    }
+
+    // Addition by Min Value
+    [TestCaseSource(nameof(AdditionTestByMinValueData))]
+    public void GeneratorDataDrivenAdditionByMinValue(decimal value1)
+    {
+        // Assert
+        Assert.Throws<OverflowException>(()=>this._calculator.Enter(decimal.MinValue).Plus(value1).Equals());
+    }
+
+// ----------------------------------------------------------------------------------------------
+
+    // Division by Max Value
+    private static IEnumerable DivisionTestData()
+    {
+        yield return new TestCaseData(0.2m);
+        yield return new TestCaseData(0.3m);
+        yield return new TestCaseData(0.30m);
+    }
+
+    [TestCaseSource(nameof(DivisionTestData))]
+    public void GeneratorDataDrivenDivision(decimal value1)
+    {
+        // Assert
         Assert.Throws<OverflowException>(()=>this._calculator.Enter(decimal.MaxValue).DivideBy(value1).Equals());
     }
 
-    private static IEnumerable MultiplicationTest()
+// --------------------------------------------------------------------------------------------
+   
+    //Divide by zero with Max value
+    private static IEnumerable DivisionTestDataByZero()
     {
-        yield return new TestCaseData(3m);
-        yield return new TestCaseData(5m);
-        yield return new TestCaseData(60m);
+        yield return new TestCaseData(0m);
     }
 
-    [TestCaseSource(nameof(MultiplicationTest))]
-    public void GeneratorMultiplication(decimal value1)
+    // use the [TestCaseSource] annotation here
+    [TestCaseSource(nameof(DivisionTestDataByZero))]
+    public void GeneratorDataDivisionByZeroWithMaxValue(decimal value1)
     {
+        // Assert
+        Assert.Throws<DivideByZeroException>(()=>this._calculator.Enter(decimal.MaxValue).DivideBy(value1));
+    }
+
+// --------------------------------------------------------------------------------------------
+
+    //Divide by zero with min value
+        private static IEnumerable DivisionTestDataByZeroMinValue()
+        {
+            yield return new TestCaseData(0m);
+        }
+
+        // use the [TestCaseSource] annotation here
+        [TestCaseSource(nameof(DivisionTestDataByZeroMinValue))]
+        public void GeneratorDataDivisionByMinValueZero(decimal value1)
+        {
+            // Assert
+            Assert.Throws<DivideByZeroException>(()=>this._calculator.Enter(decimal.MinValue).DivideBy(value1).Equals());
+    }
+
+// ---------------------------------------------------------------------------------------------------
+    //Divide with min value
+        private static IEnumerable DivisionTestDataByMinValue()
+        {
+            yield return new TestCaseData(0.5m);
+            yield return new TestCaseData(0.12m);
+            yield return new TestCaseData(0.20m);
+
+        }
+
+        // use the [TestCaseSource] annotation here
+        [TestCaseSource(nameof(DivisionTestDataByMinValue))]
+        public void GeneratorDataDivisionByMinValue(decimal value1)
+        {
+            // Assert
+            Assert.Throws<OverflowException>(()=>_calculator.Enter(decimal.MinValue).DivideBy(value1).Equals());
+    }
+
+// ---------------------------------------------------------------------------------------------------
+   
+    // Multiplication by Max value
+    private static IEnumerable? MultiplicationTestMaxValueData()
+    {
+        yield return new TestCaseData(2m);
+        yield return new TestCaseData(3m);
+        yield return new TestCaseData(30m);
+    }
+
+    
+    [TestCaseSource(nameof(MultiplicationTestMaxValueData))]
+    public void GeneratorMultiplicationMax(decimal value1)
+    {
+        // Assert
         Assert.Throws<OverflowException>(()=>this._calculator.Enter(decimal.MaxValue).MultiplyBy(value1).Equals());
     }
+    
 
-    private static IEnumerable SubtractionTest()
+// ---------------------------------------------------------------------------------------------------
+   
+    // Multiplication by Min value
+    private static IEnumerable? MultiplicationTestMinValueData()
     {
-        yield return new TestCaseData(10m);
-        yield return new TestCaseData(15m);
-        yield return new TestCaseData(160m);
+        yield return new TestCaseData(2m);
+        yield return new TestCaseData(3m);
+        yield return new TestCaseData(30m);
     }
 
-    [TestCaseSource(nameof(SubtractionTest))]
-    public void GeneratorDivision(decimal value1)
+    
+    [TestCaseSource(nameof(MultiplicationTestMinValueData))]
+    public void GeneratorMultiplicationMin(decimal value1)
     {
-        Assert.Throws<OverflowException>(()=>this._calculator.Enter(decimal.MaxValue).MinusBy(value1).Equals());
+        // Assert
+        Assert.Throws<OverflowException>(()=>this._calculator.Enter(decimal.MinValue).MultiplyBy(value1));
     }
+
+   [TearDown]
+    public void Teardown()
+    {
+        TestContext.Progress.WriteLine("This is working");
+    
+    // This method will be executed after each test.
+       _calculator.Reset();
+    }
+ 
 }
+    
